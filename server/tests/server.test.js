8e4ac6f584e1e702e2e000a4795b483952f9038d
Hilman Adinata <hilman.adinata@gmail.com>
2017-08-23 14:27:23 +0700

    add test DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -102,3 +102,48 @@ describe('GET /todos/:id', () => {
         .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var id = todos[1]._id.toHexString();
+
+        request(app)
+        .delete(`/todos/${id}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo._id).toBe(id);
+        })
+        .end((err, res) => {
+            if (err) {
+                return done(err);
+            }
+
+            Todo.findById(id).then((todo) => {
+                expect(todo).toNotExist()
+                done();
+            }).catch((e) => done(e));
+        });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var id = new ObjectID().toHexString();
+
+        request(app)
+        .delete(`/todos/${id}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 for non-object id', (done) => {
+        var id = '123';
+
+        request(app)
+        .delete(`/todos/${id}`)
+        .expect(404)
+        .end(done);
+    });
+
+    // it('shoud return 404 if todo not found', (done) => {
+    //
+    // });
+})
